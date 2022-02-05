3f138a87a4b1476513718318126fe927905e7fc8
Lorena <lzuniga1315@gmail.com>
2022-02-05 12:21:23 -0800

    created repo

---
+++ models/Category.js
@@ -0,0 +1,30 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Category extends Model {}
+
+Category.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+      // not null need to ask 
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'category',
+  }
+);
+
+module.exports = Category;
